1910d91e4ff4caf152e1ba68a634ccfdf31578c7
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-03-05 09:08:33 +0300

    #change - styles

--- src/shared/ui/form/field-wrapper/ui/FieldWrapper.tsx
+++ src/shared/ui/form/field-wrapper/ui/FieldWrapper.tsx
@@ -2,6 +2,7 @@ import { Field } from "@chakra-ui/react";
 import { useField } from "formik";
 import React from "react";
 import { FieldWrapperProps } from "../model/types";
+import s from "./FieldWrapper.module.scss";
 
 export const FieldWrapper = ({ children, name, label }: FieldWrapperProps) => {
   const [field, meta] = useField(name);
@@ -12,9 +13,7 @@ export const FieldWrapper = ({ children, name, label }: FieldWrapperProps) => {
     <Field.Root invalid={!!error}>
       <Field.Label>{label}</Field.Label>
       {React.isValidElement(children) && React.cloneElement(children, { ...field })}
-      <Field.ErrorText style={{ position: "absolute", top: "calc(100% + 2px)", lineHeight: 1.1 }}>
-        {error}
-      </Field.ErrorText>
+      <Field.ErrorText className={s.errorText}>{error}</Field.ErrorText>
     </Field.Root>
   );
 };
